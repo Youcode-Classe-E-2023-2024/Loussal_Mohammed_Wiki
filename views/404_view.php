1090c76dfa85230a17da79b0f966fd92db1d3a67
PythonScript1 <hplaptopsimo@gmail.com>
2024-01-11 18:07:02 +0100

    creation de 404 et login front

---
+++ views/404_view.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from designreset.com/cork/html/modern-dark-menu/pages-error404.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 10 Jan 2024 10:26:33 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+    <title>Error | CORK - Multipurpose Bootstrap Dashboard Template </title>
+    <link rel="icon" type="image/x-icon" href="https://designreset.com/cork/html/src/assets/img/favicon.ico"/>
+    <link href="/Loussal_Mohammed_Wiki/assets/css/adminDashboard/loader.css" rel="stylesheet" type="text/css" />
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+    <link href="/Loussal_Mohammed_Wiki/assets/css/authentification/bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+    <link href="/Loussal_Mohammed_Wiki/assets/css/adminDashboard/plugins.css" rel="stylesheet" type="text/css" />
+    <link href="/Loussal_Mohammed_Wiki/assets/css/adminDashboard/error.css" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+
+    <style>
+        body.dark .theme-logo.dark-element {
+            display: inline-block;
+        }
+        .theme-logo.dark-element {
+            display: none;
+        }
+        body.dark .theme-logo.light-element {
+            display: none;
+        }
+        .theme-logo.light-element {
+            display: inline-block;
+        }
+    </style>
+
+</head>
+<body class="error text-center">
+
+<!-- BEGIN LOADER -->
+<div id="load_screen"> <div class="loader"> <div class="loader-content">
+            <div class="spinner-grow align-self-center"></div>
+        </div></div></div>
+<!--  END LOADER -->
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4 mr-auto mt-5 text-md-left text-center">
+            <a href="index.html" class="ml-md-5">
+                <img alt="image-404" src="https://designreset.com/cork/html/src/assets/img/logo.svg" class="dark-element theme-logo">
+            </a>
+        </div>
+    </div>
+</div>
+<div class="container-fluid error-content">
+    <div class="">
+        <h1 class="error-number">404</h1>
+        <p class="mini-text">Ooops!</p>
+        <p class="error-text mb-5 mt-1">The page you requested was not found!</p>
+        <img src="https://designreset.com/cork/html/src/assets/img/error.svg" alt="cork-admin-404" class="error-img">
+        <a href="index.php?home" class="btn btn-dark mt-5">Go Back</a>
+    </div>
+</div>
+
+</body>
+
+<script src="/Loussal_Mohammed_Wiki/assets/js/adminDashboard/bootstrap.bundle.min.js"></script>
+<script src="/Loussal_Mohammed_Wiki/assets/js/adminDashboard/loader.js"></script>
+</html>
---
+++ views/login_view.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" type="text/css" href="/Loussal_Mohammed_Wiki/assets/css/authentification/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/Loussal_Mohammed_Wiki/assets/css/authentification/fontawesome-all.min.css">
+    <link rel="stylesheet" type="text/css" href="/Loussal_Mohammed_Wiki/assets/css/authentification/authentication.css">
+    <link rel="stylesheet" type="text/css" href="/Loussal_Mohammed_Wiki/assets/css/authentification/authentication_theme.css">
+</head>
+<body>
+    <div class="form-body without-side">
+        <div class="website-logo">
+            <a href="index.html">
+                <div class="logo">
+                    <img class="logo-size" src="/Loussal_Mohammed_Wiki/assets/images/logo-light.svg" alt="">
+                </div>
+            </a>
+        </div>
+        <div class="row">
+            <div class="img-holder">
+                <div class="bg"></div>
+                <div class="info-holder">
+                    <img src="/Loussal_Mohammed_Wiki/assets/images/graphic3.svg" alt="">
+                </div>
+            </div>
+            <div class="form-holder">
+                <div class="form-content">
+                    <div class="form-items">
+                        <h3>Login to account</h3>
+                        <p>Access to the most powerfull tool in the entire design and web industry.</p>
+                        <form>
+                            <div class="email_Ldiv">
+                                <input class="form-control" type="text" name="email" placeholder="E-mail Address" required>
+                            </div>
+                            <div class="password_Ldiv">
+                                <input class="form-control" type="password" name="password" placeholder="Password" required>
+                            </div>
+                            <div class="form-button">
+                                <button id="submit" type="submit" name="login" class="ibtn">Login</button> <a href="index.php?page=forget">Forget password?</a>
+                            </div>
+                        </form>
+                        <div class="other-links">
+                            <div class="text">Or login with</div>
+                            <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a><a href="#"><i class="fab fa-google"></i>Google</a><a href="#"><i class="fab fa-linkedin-in"></i>Linkedin</a>
+                        </div>
+                        <div class="page-links">
+                            <a href="index.php?page=register">Register new account</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+<script src="/Loussal_Mohammed_Wiki/assets/authentification/js/jquery.min.js"></script>
+<script src="/Loussal_Mohammed_Wiki/assets/authentification/js/popper.min.js"></script>
+<script src="/Loussal_Mohammed_Wiki/assets/authentification/js/bootstrap.min.js"></script>
+<script src="/Loussal_Mohammed_Wiki/assets/authentification/js/main.js"></script>
+</html>
