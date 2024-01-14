36a642d2cd716c5b77be893d7cc2ae1fe8513b13
PythonScript1 <loussalmohammeddev@gmail.com>
2024-01-14 21:28:21 +0100

    adding User class construct magic function to get user data using id and assign it to the properties of the class

---
+++ models/User.php
@@ -0,0 +1,29 @@
+<?php
+
+class User
+{
+    public $id;
+    public $email;
+    public $picture;
+    public $username;
+    private $password;
+
+    public function __construct($id)
+    {
+        global $db;
+
+        $stmt = $db->prepare("SELECT * FROM users WHERE user_id = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $this->id = $user['user_id'];
+        $this->picture = $user['user_picture'];
+        $this->email = $user['email'];
+        $this->username = $user['username'];
+        $this->password = $user['password'];
+    }
+
+    
+}
