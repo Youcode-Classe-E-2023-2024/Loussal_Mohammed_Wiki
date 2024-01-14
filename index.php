24c11494e7b3c2f5a5770865beab1e540275292f
PythonScript1 <loussalmohammeddev@gmail.com>
2024-01-14 21:25:09 +0100

    index.php new structure to include config file and functions for dd and db connection file and loading all model using spl autoload register

--- index.php
+++ index.php
@@ -1,17 +1,39 @@
 <?php
-include_once 'config/config.php';
+
+include_once '_config/config.php';
+include_once '_functions/functions.php';
+include_once '_config/db.php';
+
+spl_autoload_register(function ($class) {
+    include_once 'models/' . $class . '.php';
+});
+
+// Commented out as it seems you're not using these lines currently
+// $database = new Database();
+// $con = $database->dbh;
+
+
+$page = 'home';
 if (isset($_GET['page']) && !empty($_GET['page'])) {
-    $page = trim(strtolower($_GET['page']));
-} else {
-    $page = 'home';
+    $newPage = trim(strtolower($_GET['page']));
+
+    if (!(isset($_SESSION["login"]) && ($newPage == "authentication"))) {
+        $page = $newPage;
+    }
 }
 
 $all_pages = scandir('controllers');
-
 if (in_array($page . '_controller.php', $all_pages)) {
-    include_once "models/{$page}_model.php";
-    include_once "controllers/{$page}_controller.php";
-    include_once 'views/_layout.php';
+    include_once 'controllers/' . $page . '_controller.php';
+
+    if (isset($_SESSION["admin"]) && $page == "dashboard") {
+        include_once 'views/dashboard_layout.php';
+    } else {
+        include_once 'views/_layout.php';
+    }
 } else {
-    include_once '404.php';
+
+    include_once 'controllers/404_controller.php';
+    include_once 'views/404_view.php';
 }
+
