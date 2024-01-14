7c6cb7ec4d7995fb2dbc8c7669779e0a412b4ac8
PythonScript1 <loussalmohammeddev@gmail.com>
2024-01-14 22:06:36 +0100

    creating checkCategory Method to check if this category already exists

--- models/Category.php
+++ models/Category.php
@@ -33,4 +33,16 @@ class Category {
         return $stmt->fetchAll();
     }
     
+    static function CheckCategory( $category) {
+        global $db;
+        $sql = "SELECT * FROM category WHERE category = :category";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':category', $category);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($result)
+            return $result;
+        return false;
+    }
 }
