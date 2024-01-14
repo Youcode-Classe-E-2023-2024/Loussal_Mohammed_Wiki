89fc545406e9102730c3ce63a4cf582c615b3af2
PythonScript1 <loussalmohammeddev@gmail.com>
2024-01-14 22:02:59 +0100

    creatign wiki tag crud methods

--- models/Tag.php
+++ models/Tag.php
@@ -43,5 +43,47 @@ class Tag {
         return $stmt->fetchAll();
     }
 
-    
+    static function CheckTag( $tag) {
+        global $db;
+        $sql = "SELECT * FROM tag WHERE tag = :tag";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':tag', $tag);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($result)
+            return $result;
+        return false;
+    }
+
+    static function wiki_tag ($tag, $wikiId) {
+        global $db;
+        $sql = "INSERT INTO wiki_tag (tag_id, wiki_id) VALUES (:tag_id, :wiki_id)";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':tag_id', $tag);
+        $stmt->bindParam(':wiki_id', $wikiId);
+        $stmt->execute();
+    }
+
+    static function update_wiki_tag ($tag, $wiki_id) {
+        global $db;
+        $sql = "UPDATE wiki_tag  SET tag_id = :tag_id WHERE wiki_id = :wiki_id AND tag_id = :tag_id";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':tag_id', $tag);
+        $stmt->bindParam(':wiki_id', $wiki_id);
+        $stmt->bindParam(':tag_id', $tag);
+        $stmt->execute();
+    }
+
+
+
+    static function get_wiki_tag($wiki_id) {
+        global $db;
+        $sql = "SELECT tag.* FROM wiki_tag
+                JOIN tag ON wiki_tag.tag_id = tag.tag_id
+                WHERE wiki_tag.wiki_id = ?";
+        $stmt = $db->prepare($sql);
+        $stmt->execute([$wiki_id]);
+        return $stmt->fetchAll();
+    }
 }
