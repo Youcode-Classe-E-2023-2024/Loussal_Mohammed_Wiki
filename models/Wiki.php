09c5eb0b37cfdf016ff61cbbee5f77f07a414bfb
PythonScript1 <loussalmohammeddev@gmail.com>
2024-01-14 21:42:05 +0100

    creating delete wikiusing id  method

--- models/Wiki.php
+++ models/Wiki.php
@@ -19,6 +19,36 @@ class Wiki
             Tag::wiki_tag($tag, $wikiId);
         }
     }
+    static function updateWiki($wiki_id, $tags, $title, $content, $category, $updated_date, $oldTags)
+    {
+        global $db;
+        $sql = "UPDATE wiki SET  title = :title, content = :content, category_id = :category_id, updated_date = :updated_date WHERE wiki_id = :wiki_id";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':title', $title, 2);
+        $stmt->bindParam(':content', $content, 2);
+        $stmt->bindParam(':category_id', $category, 1);
+        $stmt->bindParam(':updated_date', $updated_date, 1);
+        $stmt->bindParam(':wiki_id', $wiki_id, 1);
+        $stmt->execute();
 
+        foreach ($oldTags as $tag) {
+            Tag::deleteWiki_Tag($wiki_id);
+        }
+
+        foreach ($tags as $tag) {
+            Tag::wiki_tag($tag, $wiki_id);
+        }
+    }
+
+    static function deleteWiki($wiki_id)
+    {
+        global $db;
+        $sql = "DELETE FROM wiki WHERE wiki_id = :wiki_id";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':wiki_id', $wiki_id);
+        $stmt->execute();
+    }
+
+    
     
 }
